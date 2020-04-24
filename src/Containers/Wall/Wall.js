f6d0e652f37979d716cd055dad3cb09169b54be5
MZO9400 <hamza.hameed00@yahoo.com>
2020-04-25 00:48:20 +0500

    Add basic skeleton directory structure, Login page, Signup Page, Navigation bar.

---
+++ src/Containers/Wall/Wall.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Wall extends React.Component {
+    render() {
+        return <div>WIP</div>
+    }
+}
+
+export default Wall;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Nav from './Containers/Nav/Nav';
+import Profile from './Containers/Profile/Profile';
+import Login from './Containers/Login/Login';
+import Wall from './Containers/Wall/Wall'
+import {CssBaseline} from '@material-ui/core';
+import {Redirect, Route, BrowserRouter, Switch} from 'react-router-dom';
+import { store } from './Redux/Store';
+import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
+
+let ToRender = (
+        <Switch>
+            <Route path="/profile" component={Profile}/>
+            <Route path="/login" component={Login}/>
+            <Route path="/" component={Wall}/>
+            <Redirect to="/"/>
+        </Switch>
+)
+
+
 ReactDOM.render(
-  <React.StrictMode>
-    <div>test</div>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+        <BrowserRouter>
+            <CssBaseline/>
+            <Nav/>
+            {ToRender}
+        </BrowserRouter>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
